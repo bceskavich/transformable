5e4f12d1da799e5eee66bf7fb0a0fb7f90522f69
Billy Ceskavich <bceskavich@gmail.com>
2019-03-30 09:42:31 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Transformable.MixProject do
       app: :transformable,
       name: "Transformable",
       description: "Helpers for transforming arbitrary data into structs",
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
